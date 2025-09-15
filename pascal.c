7738d2559eb3049ba769bfc46be9c3ac2123d5f6
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-09-15 22:31:04 +0530

    updated

--- pascal.c
+++ pascal.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int num=1,i,j,k;
-    for(i=0; i<=3; i++)
+    int num=1,i,j,k,n;
+    printf("enter the number of rows");
+    scanf("%d",&n);
+    for(i=0; i<n; i++)
     {
         for(j=0; j<=i; j++)
         {
